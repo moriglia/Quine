bb4ca3bdd0971139968faad0907fd63dc9bb0239
Marco Origlia <moriglia97@gmail.com>
2026-01-07 20:25:43 +0100

    Make fort_quine.f90 look more concise and elegant
    Tested with gfortran 14.3.0

--- fort_quine.f90
+++ fort_quine.f90
@@ -1,20 +1,7 @@
 program quine
   implicit none
-  character(len=574) :: s = "program quine%c  implicit none%c  character(len=574) :: s = %s%c  character(len=1), parameter :: q = achar(34)%c  integer :: i%c  i = 1%c  do while (i <= len_trim(s))%c    if (s(i:i) == achar(37)) then%c      if (s(i+1:i+1) == 'c') then%c        write(*, '()')%c      else if (s(i+1:i+1) == 's') then%c        write(*, '(a)', advance='no') q // trim(s) // q%c      end if%c      i = i + 2%c    else%c      write(*, '(a)', advance='no') s(i:i)%c      i = i + 1%c    end if%c  end do%cend program quine"
-  character(len=1), parameter :: q = achar(34)
-  integer :: i
-  i = 1
-  do while (i <= len_trim(s))
-    if (s(i:i) == achar(37)) then
-      if (s(i+1:i+1) == 'c') then
-        write(*, '()')
-      else if (s(i+1:i+1) == 's') then
-        write(*, '(a)', advance='no') q // trim(s) // q
-      end if
-      i = i + 2
-    else
-      write(*, '(a)', advance='no') s(i:i)
-      i = i + 1
-    end if
-  end do
+  character :: lf = achar(10)
+  character :: q  = achar(39)
+  character(len=500) :: fmt = '("program quine", A, "  implicit none", A, "  character :: lf = achar(10)", A, "  character :: q  = achar(39)", A, "  character(len=500) :: fmt = ", A, A, A, A, "  print trim(fmt), lf, lf, lf, lf, q, trim(fmt), q, lf, lf", A, "end program quine")'
+  print trim(fmt), lf, lf, lf, lf, q, trim(fmt), q, lf, lf
 end program quine
